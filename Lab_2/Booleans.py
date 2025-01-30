1045a2aad0d635b29d87d70a5e89775facae0e6c
Nrdaulet <bazarbayev040@gmail.com>
2025-01-30 18:13:22 +0500

    Lab2

---
+++ Lab_2/Booleans.py
@@ -0,0 +1,12 @@
+print(bool(False))
+print(bool(None))
+print(bool(0))
+print(bool(""))
+print(bool(()))
+print(bool([]))
+print(bool({}))
+print(bool("abc"))
+print(bool(123))
+print(bool(["apple", "cherry", "banana"]))
+x = 200
+print(isinstance(x, int))
